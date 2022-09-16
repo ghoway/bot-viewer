edeaa17aaffad5de18c711235c71068cf7545836
Wahyu Hidayatullah <ghoway@gmail.com>
2022-09-16 21:08:36 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,23 @@
+echo "run: update & upgrade"
+apt upgrade -y
+apt update -y
+apt update -y --fix-missing
+
+echo "run: installing git"
+apt install git -y
+apt install git -y --fix-missing
+
+echo "run: installing zip"
+apt install zip -y
+apt install zip -y --fix-missing
+
+echo "run: clone YTView"
+git clone https://github.com/MShawon/YouTube-Viewer.git --depth 10
+cd YouTube-Viewer
+
+echo "run: setup YTView"
+apt install pip -y 
+apt install pip -y --fix-missing
+python -m pip install --upgrade pip wheel
+pip install "setuptools<59"
+pip install -r requirements.txt
